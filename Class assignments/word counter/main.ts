c831a62e70a2cc13b6abc7225f4699b00cbd2bee
Zaheer Abbas <116196142+zaheerabbas01@users.noreply.github.com>
2024-04-17 22:15:22 +0500

    file added

---
+++ Class assignments/word counter/main.ts
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+console.log(chalk.bgRedBright.bold.italic("\t WELCOME TO SIMPLE WORD COUNTER \n"));
+
+let usrInpt2;
+do {
+    const usrInpt = await inquirer.prompt({
+        name: "sentence",
+        type: "input",
+        message: chalk.bgCyanBright(" Enter sentence to count words ")
+    })
+    
+    const words = usrInpt.sentence.trim().split(" ");
+    
+    const wrdslngth = chalk.bgRedBright(` ${words.length} `)
+    console.log(chalk.bgGreenBright(`\n\t Your sentence contain ${wrdslngth} words \n`));
+
+  usrInpt2 = await inquirer.prompt({
+    name: "ext",
+    type: "list",
+    message: chalk.bgCyanBright(" want to continue or exit \n"),
+    choices: ["Continue","Exit"],
+})
+    
+} while (usrInpt2.exit == "Continue");
